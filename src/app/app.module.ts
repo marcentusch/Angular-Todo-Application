ba3f21858214ede362e03931280f47713b7f2469
christianflemmer <flemmer94@hotmail.com>
2017-05-23 10:36:05 +0200

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { TodoComponent } from "app/todo/todo.component";
+
+const appRoutes: Routes = [
+  {path: 'todo', component: TodoComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', redirectTo: '/todo', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ReactiveFormsModule
-
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
