9c734491031a4096f5307d004a49effe472e9bfa
Marc Obel <marcentusch@hotmail.com>
2017-05-23 11:07:50 +0200

    Updated about and icons

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'about',
     templateUrl: 'about.component.html',
-    styleUrls: ['about.component.css']
+    styleUrls: ['../css/style.css', "about.component.css"]
 })
 
 export class AboutComponent {
