d625f6e90b156a407fbbf4ccbaf3ec76bce82bfa
christianflemmer <flemmer94@hotmail.com>
2017-05-24 13:12:50 +0200

    fixed styling in different routes, added contact routes, added icons to routes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'about',
     templateUrl: 'about.component.html',
-    styleUrls: ['../css/style.css', "about.component.css"]
+    styleUrls: ['../css/style.css']
 })
 
 export class AboutComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }                    from '@angular/platform-browser';
+import { NgModule }                         from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
+import { HttpModule }                       from '@angular/http';
+import { RouterModule, Routes }             from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { TodoComponent } from "app/todo/todo.component";
+import { AppComponent }     from './app.component';
+import { AboutComponent }   from './about/about.component';
+import { TodoComponent }    from "app/todo/todo.component";
+import { ContactComponent } from './contact/contact.component';
 
 const appRoutes: Routes = [
   {path: 'todo', component: TodoComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
   {path: '', redirectTo: '/todo', pathMatch: 'full'}
 ];
 
@@ -18,7 +20,8 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     AboutComponent,
-    TodoComponent
+    TodoComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
